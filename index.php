dedb15780deaf67e5f636730a0722fa54e14c4e2
felixschul <felix.schul@ambient-innovation.com>
2020-04-25 15:06:30 +0200

    Adopted example for part 1

--- index.php
+++ index.php
@@ -1,18 +1,3 @@
-<?
-$db = new mysqli($_ENV['DB_HOST'],$_ENV['DB_USER'],$_ENV['DB_PASSWORD'],$_ENV['DB_NAME']); # Connect to database
-$db->query('CREATE TABLE IF NOT EXISTS visits (date DATE);'); # Init table
-$result = $db->query('SELECT COUNT(*) AS count FROM visits;'); # Select previous vitits
-$db->query('INSERT INTO visits (date) VALUES (NOW());'); # Save new visit
-if ($result) { 
-    $row = $result->fetch_object();
-    $count = $row->count;
-    $result->close();
-} else {
-    $count = 0;
-}
-$db->close();
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -28,6 +13,8 @@ $db->close();
 <body>
 <h1>Nicolas Cage loves Docker ;)</h1>
 <img src="https://www.placecage.com/400/400" />
-<p>We have <?php echo $count; ?> visitors.</p>
+<p>
+<? echo "PHP works, too" ?>
+</p>
 </body>
 </html>
