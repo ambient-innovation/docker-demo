b54b417b4598a8f56ff58e7fe0b42cdb9f202cbc
felixschul <felix.schul@ambient-innovation.com>
2020-04-29 15:23:52 +0200

    Count my own visit first

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 error_reporting(0);
 $db = new mysqli($_ENV['DB_HOST'],$_ENV['DB_USER'],$_ENV['DB_PASSWORD'],$_ENV['DB_NAME']); # Connect to database
 $db->query('CREATE TABLE IF NOT EXISTS visits (date DATE);'); # Init table
-$result = $db->query('SELECT COUNT(*) AS count FROM visits;'); # Select previous vitits
 $db->query('INSERT INTO visits (date) VALUES (NOW());'); # Save new visit
+$result = $db->query('SELECT COUNT(*) AS count FROM visits;'); # Select previous vitits
 if ($result) { 
     $row = $result->fetch_object();
     $count = $row->count;
@@ -30,6 +30,6 @@ $db->close();
 <h1>Nicolas Cage loves Docker ;)</h1>
 <img src="https://www.placecage.com/400/400" />
 <p>We have <?php echo $count; ?> visitors.</p>
-<p>This is version 1.0.2</p>
+<p>This is version 1.1.0</p>
 </body>
 </html>
